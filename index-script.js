651095b33ad4d9f5ddfc88f67a36ee3a094d1a1b
sansmr007 <sandeshthapa37@gmail.com>
2022-06-28 16:35:00 +0545

    This is my intership2 day3 classwork.

---
+++ index-script.js
@@ -0,0 +1,5 @@
+var element_header = document.createElement('header');
+var element_h1 = document.createElement('h1');
+document.body.appendChild("element_header");
+var element_h1_text = 
+console.log(element_header, element_h1);
